96314e77337a15d1542f656981dbb04974a69d3b
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-08-05 17:22:05 +0530

    Update backups.py

--- SaitamaRobot/modules/backups.py
+++ SaitamaRobot/modules/backups.py
@@ -5,23 +5,23 @@ from telegram import ParseMode, Message
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler
 
-import SaitamaRobot.modules.sql.notes_sql as sql
-from SaitamaRobot import dispatcher, LOGGER, OWNER_ID, JOIN_LOGGER, SUPPORT_CHAT
-from SaitamaRobot.__main__ import DATA_IMPORT
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.helper_funcs.alternate import typing_action
+import AsunaRobot.modules.sql.notes_sql as sql
+from AsunaRobot import dispatcher, LOGGER, OWNER_ID, JOIN_LOGGER, SUPPORT_CHAT
+from AsunaRobot.__main__ import DATA_IMPORT
+from AsunaRobot.modules.helper_funcs.chat_status import user_admin
+from AsunaRobot.modules.helper_funcs.alternate import typing_action
 
-# from SaitamaRobot.modules.rules import get_rules
-import SaitamaRobot.modules.sql.rules_sql as rulessql
+# from AsunaRobot.modules.rules import get_rules
+import AsunaRobot.modules.sql.rules_sql as rulessql
 
-# from SaitamaRobot.modules.sql import warns_sql as warnssql
-import SaitamaRobot.modules.sql.blacklist_sql as blacklistsql
-from SaitamaRobot.modules.sql import disable_sql as disabledsql
+# from AsunaRobot.modules.sql import warns_sql as warnssql
+import AsunaRobot.modules.sql.blacklist_sql as blacklistsql
+from AsunaRobot.modules.sql import disable_sql as disabledsql
 
-# from SaitamaRobot.modules.sql import cust_filters_sql as filtersql
-# import SaitamaRobot.modules.sql.welcome_sql as welcsql
-import SaitamaRobot.modules.sql.locks_sql as locksql
-from SaitamaRobot.modules.connection import connected
+# from AsunaRobot.modules.sql import cust_filters_sql as filtersql
+# import AsunaRobot.modules.sql.welcome_sql as welcsql
+import AsunaRobot.modules.sql.locks_sql as locksql
+from AsunaRobot.modules.connection import connected
 
 
 @user_admin
