583a518f1cc8a99d9ed93a12891aaee87175b168
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-08-05 18:34:04 +0530

    Update notes.py

--- SaitamaRobot/modules/notes.py
+++ SaitamaRobot/modules/notes.py
@@ -3,14 +3,14 @@ from io import BytesIO
 import random
 from typing import Optional
 
-import SaitamaRobot.modules.sql.notes_sql as sql
-from SaitamaRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
-from SaitamaRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin, connection_status
-from SaitamaRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from SaitamaRobot.modules.helper_funcs.msg_types import get_note_type
-from SaitamaRobot.modules.helper_funcs.string_handling import (
+import AsunaRobot.modules.sql.notes_sql as sql
+from AsunaRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
+from AsunaRobot.modules.disable import DisableAbleCommandHandler
+from AsunaRobot.modules.helper_funcs.handlers import MessageHandlerChecker
+from AsunaRobot.modules.helper_funcs.chat_status import user_admin, connection_status
+from AsunaRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from AsunaRobot.modules.helper_funcs.msg_types import get_note_type
+from AsunaRobot.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
 )
 from telegram import (
