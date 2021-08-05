0a809d63150c5cbb7d8df57ca45a23461fcb9ca3
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-08-05 19:17:32 +0530

    Update users_sql.py

--- SaitamaRobot/modules/sql/users_sql.py
+++ SaitamaRobot/modules/sql/users_sql.py
@@ -1,7 +1,7 @@
 import threading
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.sql import BASE, SESSION
+from AsunaRobot import dispatcher
+from AsunaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import (
     Column,
     ForeignKey,
