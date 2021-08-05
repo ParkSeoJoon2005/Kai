3d3aab977485024a4dab7b48c6b8896b840ec67a
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-08-05 18:41:13 +0530

    Update reactions.py

--- SaitamaRobot/modules/reactions.py
+++ SaitamaRobot/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from SaitamaRobot import dispatcher
-from SaitamaRobot.modules.disable import DisableAbleCommandHandler
+from AsunaRobot import dispatcher
+from AsunaRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext
 
