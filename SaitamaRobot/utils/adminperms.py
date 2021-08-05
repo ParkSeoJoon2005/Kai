7adedeb0ce0ed353ce71bb67f719eb0c43825c99
Hunting Bots <69734199+HuntingBots@users.noreply.github.com>
2021-08-05 17:09:16 +0530

    Update adminperms.py

--- SaitamaRobot/utils/adminperms.py
+++ SaitamaRobot/utils/adminperms.py
@@ -1,7 +1,7 @@
 import asyncio
 from pyrogram import filters
 from pyrogram.types import ChatPermissions, Message
-from SaitamaRobot import kp as app
+from AsunaRobot import kp as app
 
 
 async def member_permissions(chat_id: int, user_id: int):
